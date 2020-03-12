88398eed697dcf2650777e7767d27e213bdb355c
bumsyalao <alaobunmi93@gmail.com>
2020-03-12 23:56:52 +0400

    setup server request

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+// import ChatRoom from "../chat-room";
+import Form from './components/Form';
+
+const StyledApp = styled.div`
+  display: grid;
+  grid-template-rows: auto min-content;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+const App = () => {
+  const [comments, setComments] = useState([]);
+
+  const onSend = comment => {
+
+    fetch("http://localhost:3001/comments", {
+      method: "POST",
+      body: JSON.stringify({
+        name: comment.name,
+        email: comment.email,
+        comment: comment.comment
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    }).then(response => response.json())
+      .then(comment =>
+        setComments([
+          ...comments,
+          comment
+        ])
+      );
+  };
+
+useEffect(() => {
+    let url = "http://localhost:3001/comments"
+    window
+      .fetch(url)
+      .then(result => result.json())
+      .then(data => setComments(data));
+  }, []);
+
+  console.log(comments, 'wtf');
+  return (
+    <StyledApp id="app">
+      {/* <ChatRoom comments={comments} /> */}
+      <Form onSend={onSend} />
+    </StyledApp>
+  );
+};
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+  background: #f5f5f5;
+  border-top: solid thin #ccc;
+  padding: 16px;
+`;
+const StyledContainer = styled.div`
+  display: block;
+  max-width: 500px;
+  margin-right: auto;
+  margin-left: auto;
+`;
+const StyledFormControl = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const StyledInput = styled.input`
+  height: 30px;
+  margin: 5px;
+  border-radius: 5px;
+  border: solid thin #ccc;
+  padding: 16px;
+  box-sizing: border-box;
+`;
+const StyledSendButton = styled.button`
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  padding: 8px;
+  margin: 5px;
+  width: 30%;
+  background-color: #007aff;
+  box-sizing: border-box;
+  cursor: pointer;
+`;
+
+const Form = ({ onSend }) => {
+  const [newComment, setNewComment] = useState({
+    name: '',
+    email: '',
+    comment: '',
+  });
+
+  const onChange = ({ target }) => {
+    setNewComment({...newComment, [target.id]: target.value});
+  };
+
+  const onSubmit = async event => {
+    event.preventDefault();
+    console.log('new comment', newComment);
+
+    onSend(newComment);
+  };
+
+  return (
+    <StyledForm id="form" onSubmit={onSubmit} >
+      <StyledContainer>
+        <StyledFormControl>
+          <StyledInput
+            id="name"
+            type="text"
+            onChange={onChange}
+            placeholder="Name"
+            value={newComment.name}
+            required
+          />
+          <StyledInput
+            id="email"
+            type="email"
+            onChange={onChange}
+            placeholder="Email"
+            value={newComment.email}
+            required
+          />
+          <StyledInput
+            id="comment"
+            type="text"
+            onChange={onChange}
+            placeholder="Write a comment..."
+            value={newComment.comment}
+            required
+          />
+          <StyledSendButton onClick={onSubmit} id="send-button" type="submit">
+            Send
+          </StyledSendButton>
+        </StyledFormControl>
+      </StyledContainer>
+    </StyledForm>
+  );
+};
+
+Form.propTypes = {
+  onSend: PropTypes.func.isRequired
+};
+
+export default Form;
